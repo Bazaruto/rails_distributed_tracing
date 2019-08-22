7d9e0c0a111ac0e7ae728c34bfafabd9693cd365
Ignatius Du Preez <ignatius@go2africa.com>
2019-08-22 10:15:13 +0200

    added rack app

---
+++ lib/rails_distributed_tracing/rack_header_modification.rb
@@ -0,0 +1,16 @@
+require 'rails_distributed_tracing/distributed_tracing'
+
+class DeltaLogger
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+
+    @status, @headers, @response = @app.call(env)
+
+    @headers[DistributedTracing::TRACE_ID] = DistributedTracing.trace_id
+
+    [@status, @headers, @response]
+  end
+end
