c1a78ffc377620a512fcf7a752a963a52e78e430
Ignatius Du Preez <ignatius@go2africa.com>
2019-08-22 13:01:27 +0200

    added rack app

--- lib/rails_distributed_tracing/plugins/faraday.rb
+++ lib/rails_distributed_tracing/plugins/faraday.rb
@@ -1,7 +1,7 @@
 module DistributedTracing
   class FaradayMiddleware < ::Faraday::Middleware
     def call(env)
-      env[:request_headers].merge!({DistributedTracing::TRACE_ID => DistributedTracing.trace_id})
+      env[:headers].merge!({DistributedTracing::TRACE_ID => DistributedTracing.trace_id})
       @app.call(env)
     end
   end
