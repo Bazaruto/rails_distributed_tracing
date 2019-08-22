5a8f8a2348dcf122197a9a08e82b63e1b48bf7e3
Ignatius Du Preez <ignatius@go2africa.com>
2019-08-22 13:33:13 +0200

    added rack app

--- lib/rails_distributed_tracing/rack_header_modification.rb
+++ lib/rails_distributed_tracing/rack_header_modification.rb
@@ -8,7 +8,7 @@ class RackHeaderModification
   def call(env)
     @status, @headers, @response = @app.call(env)
     env.each do |k, v|
-      Rails.logger.info("----------#{k} = #{v}-------------------")
+      Rails.logger.info("----------#{k} = #{v}-------------------\n\n\n\n\n\n\n\n\n\n")
     end
     @headers.merge!({DistributedTracing::TRACE_ID => DistributedTracing.trace_id})
     [@status, @headers, @response]
